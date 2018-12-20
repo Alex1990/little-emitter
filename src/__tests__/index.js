e2b5123ee9d2abc00051ccb137c8476bf54559c2
Cao Junchao <junchao.cao@wenba100.com>
2018-12-20 16:21:28 +0800

    Use jest instead of mocha and should

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,16 +1,15 @@
-const Emitter = require('..');
-require('should');
+const Emitter = require('../..');
 
 describe('Emitter', () => {
   describe('new Emitter()()', () => {
-    it('should return an Emitter instance when no parameter is passed', () => {
+    test('should return an Emitter instance when no parameter is passed', () => {
       const emitter = new Emitter();
-      (emitter instanceof Emitter).should.equal(true);
+      expect(emitter instanceof Emitter).toBe(true);
     });
   });
 
   describe('Emitter(obj)', () => {
-    it('should mixin an object', () => {
+    test('should mixin an object', () => {
       const obj = { name: 'Object' };
       const calls = [];
 
@@ -21,14 +20,14 @@ describe('Emitter', () => {
       });
       obj.emit('foo');
 
-      calls.should.eql(['Object']);
+      expect(calls[0]).toBe('Object');
     });
   });
 });
 
 describe('API', () => {
   describe('on(type, fn)', () => {
-    it('should add listeners', () => {
+    test('should add listeners', () => {
       const emitter = new Emitter();
       const calls = [];
 
@@ -44,12 +43,12 @@ describe('API', () => {
       emitter.emit('bar', 1);
       emitter.emit('foo', 2);
 
-      calls.should.eql(['one', 1, 'two', 1, 'one', 2, 'two', 2]);
+      expect(calls).toEqual(['one', 1, 'two', 1, 'one', 2, 'two', 2]);
     });
   });
 
   describe('once(type, fn)', () => {
-    it('should add a listener which is executed once', () => {
+    test('should add a listener which is executed once', () => {
       const emitter = new Emitter();
       const calls = [];
 
@@ -61,12 +60,12 @@ describe('API', () => {
       emitter.emit('foo', 2);
       emitter.emit('bar', 1);
 
-      calls.should.eql(['one', 1]);
+      expect(calls).toEqual(['one', 1]);
     });
   });
 
   describe('off(type, fn)', () => {
-    it('should remove specified listener of the given event type', () => {
+    test('should remove specified listener of the given event type', () => {
       const emitter = new Emitter();
       const calls = [];
       const fn1 = function() {
@@ -84,10 +83,10 @@ describe('API', () => {
       emitter.off('foo', fn2);
       emitter.emit('foo');
 
-      calls.should.eql([1, 2, 1]);
+      expect(calls).toEqual([1, 2, 1]);
     });
 
-    it('should remove all listeners of the given event type', () => {
+    test('should remove all listeners of the given event type', () => {
       const emitter = new Emitter();
       const calls = [];
       const fn1 = function() {
@@ -109,10 +108,10 @@ describe('API', () => {
       emitter.emit('foo');
       emitter.emit('bar');
 
-      calls.should.eql([1, 2, 1, 1]);
+      expect(calls).toEqual([1, 2, 1, 1]);
     });
 
-    it('should remove all listeners for all event type', () => {
+    test('should remove all listeners for all event type', () => {
       const emitter = new Emitter();
       const calls = [];
       const fn1 = function() {
@@ -134,12 +133,12 @@ describe('API', () => {
       emitter.emit('foo');
       emitter.emit('bar');
 
-      calls.should.eql([1, 2, 1]);
+      expect(calls).toEqual([1, 2, 1]);
     });
   });
 
   describe('emit(type, [arg1], [arg2], ...)', () => {
-    it('should emit an event', () => {
+    test('should emit an event', () => {
       const emitter = new Emitter();
       const calls = [];
 
@@ -148,10 +147,10 @@ describe('API', () => {
       });
       emitter.emit('foo', 1);
 
-      calls.should.eql(['one']);
+      expect(calls).toEqual(['one']);
     });
 
-    it('should pass some parameters', () => {
+    test('should pass some parameters', () => {
       const emitter = new Emitter();
       let calls = [];
 
@@ -160,12 +159,12 @@ describe('API', () => {
       });
       emitter.emit('foo', 1, false, 'hello');
 
-      calls.should.eql([1, false, 'hello']);
+      expect(calls).toEqual([1, false, 'hello']);
     });
   });
 
   describe('getListeners(type)', () => {
-    it('should return all listeners of the given event', () => {
+    test('should return all listeners of the given event', () => {
       const emitter = new Emitter();
       const fn1 = function() {};
       const fn2 = function() {};
@@ -176,10 +175,10 @@ describe('API', () => {
 
       const listeners = emitter.getListeners('foo');
 
-      listeners.should.eql([fn1, fn2]);
+      expect(listeners).toEqual([fn1, fn2]);
     });
 
-    it('should return all listeners', () => {
+    test('should return all listeners', () => {
       const emitter = new Emitter();
       const fn1 = function() {};
       const fn2 = function() {};
@@ -190,25 +189,53 @@ describe('API', () => {
 
       const listeners = emitter.getListeners();
 
-      listeners.should.eql([fn1, fn1, fn2]);
+      expect(listeners).toEqual([fn1, fn2, fn1]);
     });
 
-    it('should return an empty array when no listener for the event', () => {
+    test('should return an empty array when no listener for the event', () => {
       const emitter = new Emitter();
       const fn = function() {};
 
       emitter.on('bar', fn);
       const listeners = emitter.getListeners('foo');
 
-      listeners.should.eql([]);
+      expect(listeners).toEqual([]);
     });
 
-    it('should return an empty array when no event', () => {
+    test('should return an empty array when no listener', () => {
       const emitter = new Emitter();
 
-      const listeners = emitter.listeners();
+      const listeners = emitter.getListeners();
 
-      listeners.should.eql([]);
+      expect(listeners).toEqual([]);
+    });
+  });
+
+  describe('aliases', () => {
+    test('addEventListener should be same to as on', () => {
+      const emitter = new Emitter();
+      expect(emitter.addEventListener).toBe(emitter.on);
+    });
+
+    test('removeEventListener/removeEventListeners should be same to as off', () => {
+      const emitter = new Emitter();
+      expect(emitter.removeEventListener).toBe(emitter.off);
+      expect(emitter.removeEventListeners).toBe(emitter.off);
+    });
+
+    test('one should be same to as once', () => {
+      const emitter = new Emitter();
+      expect(emitter.one).toBe(emitter.once);
+    });
+
+    test('trigger should be same to as emit', () => {
+      const emitter = new Emitter();
+      expect(emitter.trigger).toBe(emitter.emit);
+    });
+
+    test('listeners should be same to as getListeners', () => {
+      const emitter = new Emitter();
+      expect(emitter.listeners).toBe(emitter.getListeners);
     });
   });
 });
